67b2023098ddabccda1c9f093ff876c08421c95a
martoff <martoff@ukr.net>
2026-04-04 03:17:57 +0300

    'Solution'

--- services/user.py
+++ services/user.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 User = get_user_model()
+
+
+def get_user(*, user_id: int) -> models.Model:
+    return User.objects.get(id=user_id)
 
 
 def create_user(
@@ -30,30 +35,19 @@ def create_user(
     return user
 
 
-def get_user(user_id: int) -> models.Model:
-    return User.objects.get(id=user_id)
-
-
 def update_user(
     user_id: int,
-    username: str = None,
-    password: str = None,
-    email: str = None,
-    first_name: str = None,
-    last_name: str = None
+    **kwargs
 ) -> models.Model:
-    user = get_user(id=user_id)
+    user = get_user(user_id=user_id)
 
-    if username:
-        user.username = username
-    if email:
-        user.email = email
-    if first_name:
-        user.first_name = first_name
-    if last_name:
-        user.last_name = last_name
-    if password:
-        user.set_password(password)
+    for field in ["username", "password", "email", "first_name", "last_name"]:
+        if field in kwargs and kwargs[field] is not None:
+            if field == "password":
+                user.set_password(kwargs[field])
+
+            else:
+                setattr(user, field, kwargs[field])
 
     user.save()
     return user
